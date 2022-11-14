0f61d1cf0d127b756e0de680de3650fb9cd923d9
Sharon Tieu <tieusharon@gmail.com>
2022-11-14 17:33:49 +0000

    Completed feature-two-user-can-sign-in.

---
+++ client/components/home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import NavBar from '../components/nav-bar';
+import SignUpForm from '../components/create-account';
+import SignInForm from '../components/sign-in';
+// import CharacterCreation from '../components/character-creation';
+import './styles.css';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 'register'
+    };
+    this.pageNavigate = this.pageNavigate.bind(this);
+  }
+
+  pageNavigate(route) {
+    if (route === 'sign-in') {
+      this.setState({ currentPage: 'sign-in' });
+    } else {
+      this.setState({ currentPage: 'register' });
+    }
+  }
+
+  render() {
+    console.log('this.state:', this.state);
+    return (
+      <div className="h-100 bg-purple">
+        <NavBar pageNavigate={ this.pageNavigate } />
+        { this.state.currentPage === 'register' ? <SignUpForm /> : this.state.currentPage === 'sign-in' ? <SignInForm /> : null }
+      </div>
+      // <CharacterCreation />
+    );
+  }
+}
